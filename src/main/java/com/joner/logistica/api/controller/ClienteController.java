34c974b794dd13073912e84a6ec5a52c680f7644
Gustavo Joner <gustavo.joner@hotmail.com>
2022-11-02 16:42:51 -0300

    repository, exceptions, service and scripts

--- src/main/java/com/joner/logistica/api/controller/ClienteController.java
+++ src/main/java/com/joner/logistica/api/controller/ClienteController.java
@@ -1,30 +1,74 @@
 package com.joner.logistica.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joner.logistica.domain.model.Cliente;
+import com.joner.logistica.domain.repository.ClienteRepository;
+import com.joner.logistica.domain.service.CatalogoClienteService;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping("/clientes")
+	private ClienteRepository clienteRepository;
+	
+	private CatalogoClienteService catalogoClienteService;
+	
+	@GetMapping
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setEmail("joaorosa@gmail.com");
-		cliente1.setTelefone("987654321");
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/{clientId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clientId) {
+		return clienteRepository.findById(clientId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+		return catalogoClienteService.salvar(cliente);
+	}
+	
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clientId,@Valid @RequestBody Cliente cliente) {
+		if(!clienteRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("mariarosa@gmail.com");
-		cliente2.setTelefone("987654321");
+		cliente.setId(clientId);
+		cliente = catalogoClienteService.salvar(cliente);
 		
-		return Arrays.asList(cliente1, cliente2);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clientId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clientId) {
+		if(!clienteRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		catalogoClienteService.excluir(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
